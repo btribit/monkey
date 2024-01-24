f1202450541e52d13a4e192afab7fa5572bfe9cf
btribit <bob@tribit.io>
2024-01-24 16:07:54 -0600

    Some evaluator bits.

---
+++ evaluator/evaluator.go
@@ -0,0 +1,89 @@
+package evaluator
+
+import (
+	"monkey/ast"
+	"monkey/object"
+)
+
+// Define constants for the Boolean object
+var (
+	TRUE  = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+	NULL  = &object.Null{}
+)
+
+// Eval is a function that evaluates an AST node
+func Eval(node ast.Node) object.Object {
+	switch node := node.(type) {
+	// Statements
+	case *ast.Program:
+		return evalStatements(node.Statements)
+
+	case *ast.ExpressionStatement:
+		return Eval(node.Expression)
+
+	// Expressions
+	case *ast.IntegerLiteral:
+		return &object.Integer{Value: node.Value}
+
+	case *ast.Boolean:
+		return nativeBoolToBooleanObject(node.Value)
+
+	case *ast.PrefixExpression:
+		right := Eval(node.Right)
+		return evalPrefixExpression(node.Operator, right)
+	}
+
+	return nil
+}
+
+// evalStatements is a helper function that takes in a slice of statements and
+// evaluates each statement in the slice
+func evalStatements(stmts []ast.Statement) object.Object {
+	var result object.Object
+
+	for _, statement := range stmts {
+		result = Eval(statement)
+	}
+
+	return result
+}
+
+// nativeBoolToBooleanObject is a helper function that takes in a boolean and
+// returns a pointer to a Boolean object
+func nativeBoolToBooleanObject(input bool) *object.Boolean {
+	if input {
+		return TRUE
+	}
+	return FALSE
+}
+
+// evalPrefixExpression is a helper function that takes in an operator and an
+// object and evaluates the prefix expression
+func evalPrefixExpression(operator string, right object.Object) object.Object {
+	switch operator {
+	case "!":
+		return evalBangOperatorExpression(right)
+
+	default:
+		return NULL
+	}
+}
+
+// evalBangOperatorExpression is a helper function that takes in an object and
+// evaluates the bang operator expression
+func evalBangOperatorExpression(right object.Object) object.Object {
+	switch right {
+	case TRUE:
+		return FALSE
+
+	case FALSE:
+		return TRUE
+
+	case NULL:
+		return TRUE
+
+	default:
+		return FALSE
+	}
+}
---
+++ evaluator/evaluator_test.go
@@ -0,0 +1,109 @@
+package evaluator
+
+import (
+	"monkey/lexer"
+	"monkey/object"
+	"monkey/parser"
+	"testing"
+)
+
+func TestEvalIntegerExpression(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected int64
+	}{
+		{"5", 5},
+		{"10", 10},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testIntegerObject(t, evaluated, tt.expected)
+	}
+}
+
+// TestEvalBooleanExpression is a function that tests the evaluation of boolean
+// expressions
+func TestEvalBooleanExpression(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected bool
+	}{
+		// Boolean expressions
+		{"true", true},
+		{"false", false},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testBooleanObject(t, evaluated, tt.expected)
+	}
+}
+
+// TestBangOperator is a function that tests the evaluation of the bang operator
+func TestBangOperator(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected bool
+	}{
+		// Boolean expressions
+		{"!true", false},
+		{"!false", true},
+		// Integer expressions
+		{"!5", false},
+		{"!!5", true},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testBooleanObject(t, evaluated, tt.expected)
+	}
+}
+
+// testEval is a helper function that takes in a string and returns the evaluated
+// object
+func testEval(input string) object.Object {
+	l := lexer.New(input)
+	p := parser.New(l)
+	program := p.ParseProgram()
+
+	return Eval(program)
+}
+
+// testIntegerObject is a helper function that takes in a testing object, an
+// object, and an integer. It tests whether the object is an integer and whether
+// the integer is equal to the expected integer.
+func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
+	result, ok := obj.(*object.Integer)
+	if !ok {
+		t.Errorf("object is not an Integer. got=%T (%+v)", obj, obj)
+		return false
+	}
+
+	if result.Value != expected {
+		t.Errorf("object has wrong value. got=%d, want=%d",
+			result.Value, expected)
+		return false
+	}
+
+	return true
+}
+
+// testBooleanObject is a helper function that takes in a testing object, an
+// object, and a boolean. It tests whether the object is a boolean and whether
+// the boolean is equal to the expected boolean.
+func testBooleanObject(t *testing.T, obj object.Object, expected bool) bool {
+	result, ok := obj.(*object.Boolean)
+	if !ok {
+		t.Errorf("object is not a Boolean. got=%T (%+v)", obj, obj)
+		return false
+	}
+
+	if result.Value != expected {
+		t.Errorf("object has wrong value. got=%t, want=%t",
+			result.Value, expected)
+		return false
+	}
+
+	return true
+}
