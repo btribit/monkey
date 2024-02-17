782434e1f8572e2c5acef6cb4876dcd9d5c2ff53
btribit <bob@tribit.io>
2024-02-16 18:37:26 -0600

    Working compiler. For constants.

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -21,7 +21,59 @@ func New() *compiler {
 }
 
 func (c *compiler) Compile(node ast.Node) error {
+	switch node := node.(type) {
+	case *ast.Program:
+		for _, s := range node.Statements {
+			err := c.Compile(s)
+			if err != nil {
+				return err
+			}
+		}
+
+	case *ast.ExpressionStatement:
+		err := c.Compile(node.Expression)
+		if err != nil {
+			return err
+		}
+
+	case *ast.InfixExpression:
+		err := c.Compile(node.Left)
+		if err != nil {
+			return err
+		}
+
+		err = c.Compile(node.Right)
+		if err != nil {
+			return err
+		}
+
+	case *ast.IntegerLiteral:
+		integer := &object.Integer{Value: node.Value}
+		c.emit(code.OpConstant, c.addConstant(integer))
+
+	}
+
 	return nil
+}
+
+// addConstant adds a constant to the compiler's constant pool and returns its position
+func (c *compiler) addConstant(obj object.Object) int {
+	c.constants = append(c.constants, obj)
+	return len(c.constants) - 1
+}
+
+// emit appends the given instructions to the compiler's instruction stream
+func (c *compiler) emit(op code.Opcode, operands ...int) int {
+	instruction := code.Make(op, operands...)
+	position := c.addInstruction(instruction)
+	return position
+}
+
+// addInstruction adds the given instructions to the compiler's instruction stream
+func (c *compiler) addInstruction(ins []byte) int {
+	positionNewInstruction := len(c.instructions)
+	c.instructions = append(c.instructions, ins...)
+	return positionNewInstruction
 }
 
 func (c *compiler) Bytecode() *Bytecode {
