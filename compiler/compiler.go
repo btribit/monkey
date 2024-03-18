dd69109fd9177f563e8cef0243dd9a64e8bc2989
Bob Tribit <bob@tribit.io>
2024-03-18 02:22:43 +0200

    Adding arguments to function calls in the compiler.

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -291,7 +291,14 @@ func (c *compiler) Compile(node ast.Node) error {
 			return err
 		}
 
-		c.emit(code.OpCall)
+		for _, arg := range node.Arguments {
+			err := c.Compile(arg)
+			if err != nil {
+				return err
+			}
+		}
+
+		c.emit(code.OpCall, len(node.Arguments))
 	}
 
 	return nil
